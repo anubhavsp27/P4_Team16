068e1a9ec6bb38ce4c38c13eb79735bab4b3a775
csanford3 <csanford4@wisc.edu>
2018-04-04 20:34:57 -0500

    Added Graphnode class to Graph.java

---
+++ src/Graph.java
@@ -0,0 +1,88 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import Graph.Graphnode;
+
+/**
+ * Undirected and unweighted graph implementation
+ * 
+ * @param <E> type of a vertex
+ * 
+ * @author sapan (sapan@cs.wisc.edu)
+ * 
+ */
+public class Graph<E> implements GraphADT<E> {
+    
+	class Graphnode<T> {
+	    // *** fields ***
+	    private T data;
+	    private List<Graphnode<T>> successors = new ArrayList<Graphnode<T>>();
+	 
+	    // *** methods ***
+	    
+	}
+	
+    /**
+     * Instance variables and constructors
+     */
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public E addVertex(E vertex) {
+        
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public E removeVertex(E vertex) {
+        
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean addEdge(E vertex1, E vertex2) {
+        
+    }    
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean removeEdge(E vertex1, E vertex2) {
+        
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean isAdjacent(E vertex1, E vertex2) {
+        
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Iterable<E> getNeighbors(E vertex) {
+        
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Iterable<E> getAllVertices() {
+        
+    }
+
+}
