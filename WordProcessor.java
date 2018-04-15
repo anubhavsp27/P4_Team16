8e521b5a8ec47c63e5306710a8e7c0128a8b9c9d
anubhavsp27 <37089852+anubhavsp27@users.noreply.github.com>
2018-04-14 23:12:08 -0500

    WordProcessor.java

--- WordProcessor.java
+++ WordProcessor.java
@@ -64,8 +64,10 @@ public class WordProcessor {
 		 * Note: since map and filter return the updated Stream objects, they can chained together as:
 		 * 		streamOfLines.map(...).filter(a -> ...).map(...) and so on
 		 */
-		
-		return null;
+		Stream<String> streamofLines = Files.lines(Paths.get(filepath));
+		streamofLines.map(String::trim).filter(x -> ! x.equals(""));
+		streamofLines.map(String::toUpperCase);
+		return streamofLines;
 	}
 	
 	/**
@@ -86,6 +88,84 @@ public class WordProcessor {
 	 * @return true if word1 and word2 are adjacent else false
 	 */
 	public static boolean isAdjacent(String word1, String word2) {
+		// wordLengthDiff stores the absolute difference between
+		// in the length of the two words
+		
+		int wordLengthDiff = Math.abs(word2.length()-word1.length());
+		
+		// For the two words to be adjacent, the should either have the
+		// same number of characters or differ by at most one character:
+		
+		if (wordLengthDiff > 1)
+		    return false;
+		
+		else if (wordLengthDiff == 0){
+			// Since in this case, both words have the same number of character 
+			// (i.e. wordLengthDiff=0),for them to be adjacent, there must 
+			// be a difference of only one character
+			
+		    int count = 0;  // counts the number of characters that differ
+		                   // between the two words.
+		    for (int i = 0; i < word1.length(); i++ ) {
+		    	    if(word1.charAt(i) != word2.charAt(i)) {
+		    	    	    count++;
+		    	    	    
+		    	    	    // If the number of characters at the same position
+		    	    	    // of the two words that do not match (as tracked by
+		    	    	    // count) exceeds 1, the words are not adjacent:
+		    	    	    
+		    	    	    if(count > 1)
+		    	    	    	    return false;
+		          }   
+		    } 
+		    return true;    
+		}
+		
+		else if (wordLengthDiff == 1) {
+			// The words will be adjacent in this case only if there is
+			// an addition/removal of one character while the relative 
+			// position of all other characters remain the same.
+			
+			int count = 0;  // counts the number of characters at the same
+			              // position that differ between the two words
+			
+			String longerWord = word1;  // Word with greater length 
+			                            // initialized to word1
+			String shorterWord = word2;
+			
+			// Assigning longer and shorter words:
+			if (word2.length() > word1.length()) {
+				longerWord=word2;
+				shorterWord=word1;
+			}
+			int j = 0;  // used to access characters of longerWord
+			            // in the following for loop
+			for (int i = 0; i < shorterWord.length(); i++) {
+				if (shorterWord.charAt(i) != longerWord.charAt(j)) {
+					// If the characters at a position do not match:
+					// First, count is incremented
+					count++;
+					// For words to be adjacent, no more than one character
+					// at the same position should differ. Thus:
+					if (count > 1)
+						return false;
+					// Next, 'i' is decremented so that the current character
+					// of shorterWord is compared to the next character of 
+					// longerWord during the next iteration.
+					i--;
+				}
+				j++;
+			}
+			// The above for-loop iterates until the last character
+			// of shorterWord is reached. Thus the last character of
+			// longerWord may not be compared with any characters of
+			// shorterWord in some cases. In these cases,the two words
+			// must be adjacent and the additional character added to
+			// the shorter word must be at the end since the difference 
+			// the length of the two words is 1.
+			return true;
+		}
+		
 		return false;	
 	}
 	
