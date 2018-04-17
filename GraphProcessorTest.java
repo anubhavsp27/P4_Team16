90b8ccf446670cf0f4adef9bad9a634dc5f8156f
anubhavsp27 <37089852+anubhavsp27@users.noreply.github.com>
2018-04-16 20:26:09 -0500

    Add files via upload

--- GraphProcessorTest.java
+++ GraphProcessorTest.java
@@ -1,39 +1,4 @@
-import static org.junit.Assert.*;
-
-import org.junit.BeforeClass;
-import org.junit.Test;
 
 public class GraphProcessorTest {
-	
-	GraphProcessor graphPrc = new GraphProcessor();
-	
-	
-
-	@Test
-	public void populateGraphWithTwentyWords() {
-		
-		int testInt = graphPrc.populateGraph("twentyWords.txt");
-		
-		assertEquals(20, testInt);
-		
-	
-	}
-	
-	@Test 
-	public void populateGraphShortestPath() {
-		
-		int testInt = graphPrc.populateGraph("shortestPathTest.txt");
-		assertEquals(3, testInt);
-	}
-	
-	@Test
-	public void getShortestPath() {
-		
-		graphPrc.populateGraph("shortestPathTest.txt");
-		graphPrc.shortestPathPrecomputation();
-		int shortestDistance = graphPrc.getShortestDistance("cat", "hath");
-		assertEquals(2, shortestDistance);
-		
-	}
 
 }
